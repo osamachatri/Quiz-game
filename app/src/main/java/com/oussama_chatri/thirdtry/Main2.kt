a2ea298e4acff22f56b2de5b929d271ae7e2e248
unknown <oussamachatri7@gmail.com>
2024-02-21 23:04:18 +0100

    second commit

---
+++ app/src/main/java/com/oussama_chatri/thirdtry/Main2.kt
@@ -0,0 +1,11 @@
+package com.oussama_chatri.thirdtry
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Main2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main2)
+    }
+}
